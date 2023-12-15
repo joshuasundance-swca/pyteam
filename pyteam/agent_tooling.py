c4dda8bb088a061a51361b92acad02b70b401533
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-12-15 17:45:14 -0500

    agent

--- pyteam/agent_tooling.py
+++ pyteam/agent_tooling.py
@@ -1,60 +1,33 @@
-from typing import Optional
-
 from langchain.agents import AgentExecutor
 from langchain.agents import AgentType, initialize_agent
 from langchain.agents.tools import Tool
 from langchain.llms.base import BaseLLM
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import MessagesPlaceholder
-from langchain.schema.runnable import Runnable
-from langchain.tools import tool
 
-from pyteam.fleet_retrievers import MultiVectorFleetRetriever
 from pyteam.fleet_specialists import FleetBackedSpecialist
 
 
-def request_specialist(
-    library_name: str,
-    llm: BaseLLM,
-    download_kwargs: Optional[dict] = None,
-) -> Runnable:
-    retriever = MultiVectorFleetRetriever.from_library(library_name, download_kwargs)
-    return FleetBackedSpecialist(library_name, retriever, llm).specialist
-
-
-llm = ...
-download_kwargs = dict(cache_dir="fleet-cache")
-
-tools = []
-
-
-@tool("add_specialist", "Add a specialist for a specific Python library")
-def add_specialist(library_name: str) -> str:
-    global tools
-    retriever = MultiVectorFleetRetriever.from_library(library_name, download_kwargs)
-    specialist = FleetBackedSpecialist(library_name, retriever, llm).specialist
-    specialist_tool = Tool.from_function(
-        specialist.invoke,
-        name=f"{library_name} specialist",
-        description=f"Specialist for {library_name}",
-    )
-    tools.append(specialist_tool)
-    return specialist_tool.name
-
-
-class ToolGrabber:
+class SpecialistBackedAgent:
     tools: list[Tool]
+    llm: BaseLLM
+    cache_dir: str = "fleet-cache"
     agent: AgentExecutor
 
-    def add_tool(self, tool_name: str) -> str:
+    def _summon_specialist(self, library_name: str) -> str:
+        specialist = FleetBackedSpecialist.from_library(
+            library_name,
+            self.llm,
+            dict(cache_dir=self.cache_dir),
+        ).specialist
         tool = Tool.from_function(
-            lambda s: "WOOF WOOF WOOF WOOF",
-            name=tool_name,
-            description=f"Tool for {tool_name}",
+            specialist.invoke,
+            name=library_name,
+            description=f"Get advice from a {library_name} specialist",
             return_direct=True,
         )
         self.tools.append(tool)
-        self.agent = self.get_agent_executor(llm, self.agent.memory)
+        self.agent = self.get_agent_executor(self.llm, self.agent.memory)
         return tool.name
 
     def get_agent_executor(
@@ -76,11 +49,13 @@ class ToolGrabber:
         )
 
     def __init__(self, llm: BaseLLM):
+        self.llm = llm
+
         self.tools = [
             Tool.from_function(
-                self.add_tool,
-                name="create_tool",
-                description="Create a tool (just give it a name)",
+                self._summon_specialist,
+                name="summon_specialist",
+                description="Summon a specialist for a Python library",
             ),
         ]
 
