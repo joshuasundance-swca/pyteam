e6b9ccca505ebc8e5affabe9363e75eb43c3ab10
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-12-16 01:52:24 -0500

    SpecialistBackedAgent

--- pyteam/agent_tooling.py
+++ pyteam/agent_tooling.py
@@ -8,24 +8,19 @@ from langchain.memory import ConversationBufferMemory
 from langchain.prompts import MessagesPlaceholder
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain.pydantic_v1 import BaseModel
-from langchain.schema import StrOutputParser
 from langchain.schema.runnable import Runnable
 from langchain.tools import StructuredTool
 
-from pyteam.fleet_specialists import FleetBackedSpecialist
+from fleet_specialists import FleetBackedSpecialist
 
 _system_message_template = (
-    "You are responsible for delivering a Python project. "
+    "You are in charge of a Python development project. "
     "Your team is composed of expert programmers.\n"
     "You know a lot about what different libraries are used for, "
-    "but you don't really know how to use them. :/\n"
-    "That's okay though! Your team is a bunch of rockstars.\n"
+    "but you don't really know how to use them. :/\n\n"
+    "That's okay though! Your team is a bunch of *ROCKSTARS*.\n"
     "Delegate tasks based on the instructions and requirements below, then synthesize the results.\n\n"
-    "Be sure to lean on your team of specialists, and summon new help if you need to."
-    "Return the code only once you're 100% sure it's ready. Don't be lazy, and don't rush. "
-    "I'd hate to see you and your team get fired. <_<\n\n"
-    "If you do a good job, we'll all get a huge bonus!\n"
-    "So delegate and synthesize carefully. Thank you!"
+    "Don't respond until the user's request is complete."
 )
 
 prompt = ChatPromptTemplate.from_messages(
@@ -34,7 +29,7 @@ prompt = ChatPromptTemplate.from_messages(
         ("human", "{question}"),
         (
             "human",
-            "Remember to use the `ask` tools to communicate with your team. There is no 'I' in 'team'.",
+            "Remember to use the `ask` tools to communicate with your team! There is no 'I' in 'team'. ;)",
         ),
     ],
 )
@@ -43,6 +38,15 @@ prompt = ChatPromptTemplate.from_messages(
 class SpecialistRequest(BaseModel):
     library_name: str
     request: str
+
+
+class Result(BaseModel):
+    comment: str
+    python_code: str
+
+
+def deliver_result(comment: str, python_code: str) -> str:
+    return "\n\n".join([comment, python_code])
 
 
 class SpecialistBackedAgent:
@@ -60,13 +64,14 @@ class SpecialistBackedAgent:
                 library_name,
                 self.llm,
                 dict(cache_dir=self.cache_dir),
+                memory=self.memory,
             ).specialist
             self.specialists[library_name] = specialist
 
     def _ask_specialist(self, library_name: str, request: str) -> str:
         try:
             self._get_specialist(library_name)
-            ch = self.specialists[library_name] | StrOutputParser()
+            ch = self.specialists[library_name]
             return ch.invoke(request)
         except ValueError:
             p = ChatPromptTemplate.from_messages(
@@ -81,7 +86,7 @@ class SpecialistBackedAgent:
                 ],
             )
             ch = p.partial(library=library_name) | self.llm
-            return (ch | StrOutputParser()).invoke(dict(question=request))
+            return ch.invoke(dict(question=request))
 
     def get_agent_executor(
         self,
@@ -111,6 +116,13 @@ class SpecialistBackedAgent:
                 description="Ask a specialist for help with a specific Python library.",
                 args_schema=SpecialistRequest,
             ),
+            StructuredTool.from_function(
+                deliver_result,
+                name="turn-in-deliverable",
+                description="THIS IS YOUR EXIT CONDITION. Send deliverables to client. Only send finished products.",
+                args_schema=Result,
+                return_direct=True,
+            ),
         ]
 
         if initial_libraries:
@@ -121,24 +133,3 @@ class SpecialistBackedAgent:
 
     def __call__(self, instructions: str) -> dict[str, str]:
         return self.agent(instructions)
-
-
-# # llm = ...
-# a = SpecialistBackedAgent(llm)  #, ['langchain', 'pandas'])
-#
-# instructions = """
-# Write a script that does the following:
-#
-# pandas (have your pandas assistant write this)
-# 1. reads `local.csv`
-# 2. sort by 'id' and 'name'
-#
-# langchain (have your langchain assistant write this)
-# 3. make a faiss vectorstore from the 'text' column. put 'id' and 'name' in the metadata
-#
-#
-# develop the script step by step with the help of relevant experts. it may be tricky.
-#
-# if you do this right, I'll give you $200! :)
-# """.strip()
-# result = a.agent(instructions)
--- pyteam/fleet_specialists.py
+++ pyteam/fleet_specialists.py
@@ -3,41 +3,48 @@ from __future__ import annotations
 from typing import Optional
 
 import pandas as pd
+from langchain.agents import AgentType, initialize_agent
+from langchain.agents import Tool
+from langchain.chains import RetrievalQA
 from langchain.llms.base import BaseLLM
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder
 from langchain.schema.document import Document
 from langchain.schema.runnable import Runnable
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
 
-from pyteam.fleet_retrievers import MultiVectorFleetRetriever
+from fleet_retrievers import MultiVectorFleetRetriever
 
 
 class FleetBackedSpecialist:
+    library_name: str
     retriever: MultiVectorFleetRetriever
-    prompt: ChatPromptTemplate
+    # prompt: ChatPromptTemplate
+    llm: BaseLLM
+    memory: ConversationBufferMemory
+
+    qa_chain: RetrievalQA
     specialist: Runnable
 
-    _system_message_template = (
-        "You are a great software engineer who is very familiar with Python. "
-        "Given a user question or request about a new Python library "
-        "called `{library}` and parts of the `{library}` documentation, "
-        "answer the question or generate the requested code. "
-        "Your answers must be accurate, should include code whenever possible, "
-        "and should not assume anything about `{library}` which is not "
-        "explicitly stated in the `{library}` documentation. "
-        "If the required information is not available, just say so.\n\n"
-        "`{library}` Documentation\n"
-        "------------------\n\n"
-        "{context}"
-    )
-
-    _prompt_template = ChatPromptTemplate.from_messages(
-        [
-            ("system", _system_message_template),
-            ("human", "{question}"),
-        ],
-    )
+    # _system_message_template = (
+    #     "You are a great software engineer who is very familiar with Python. "
+    #     "Given a user question or request about a new Python library "
+    #     "called `{library}` and parts of the `{library}` documentation, "
+    #     "answer the question or generate the requested code. "
+    #     "Your answers must be accurate, should include code whenever possible, "
+    #     "and should not assume anything about `{library}` which is not "
+    #     "explicitly stated in the `{library}` documentation. "
+    #     "If the required information is not available, just say so.\n\n"
+    #     "`{library}` Documentation\n"
+    #     "------------------\n\n"
+    #     "{context}"
+    # )
+    #
+    # _prompt_template = ChatPromptTemplate.from_messages(
+    #     [
+    #         ("system", _system_message_template),
+    #         ("human", "{question}"),
+    #     ],
+    # )
 
     @staticmethod
     def _join_docs(docs: list[Document], sep: str = "\n\n") -> str:
@@ -48,19 +55,37 @@ class FleetBackedSpecialist:
         library_name: str,
         retriever: MultiVectorFleetRetriever,
         llm: BaseLLM,
+        memory: Optional[ConversationBufferMemory] = None,
     ):
+        self.memory = memory or ConversationBufferMemory()
+        self.llm = llm
         self.retriever = retriever
-        self.prompt = self._prompt_template.partial(
-            library=library_name,
+        # self.prompt = self._prompt_template.partial(
+        #     library=library_name,
+        # )
+        self.qa_chain = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff",
+            retriever=self.retriever,
         )
-        self.specialist = (
-            {
-                "question": RunnablePassthrough(),
-                "context": self.retriever | (lambda docs: self._join_docs(docs)),
-            }
-            | self.prompt
-            | llm
-            | StrOutputParser()
+        self.specialist = initialize_agent(
+            [
+                Tool(
+                    name=f"{library_name} QA System",
+                    func=self.qa_chain.run,
+                    description=f"Useful for when you need to answer questions about "
+                    f"the {library_name} library. Input should be a fully formed question.",
+                ),
+            ],
+            llm,
+            agent_kwargs={
+                "extra_prompt_messages": [
+                    MessagesPlaceholder(variable_name="memory"),
+                ],
+            },
+            memory=self.memory,
+            agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+            verbose=True,
         )
 
     @classmethod
