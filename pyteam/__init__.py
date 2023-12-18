c7dcc84e5fdeb9ccdeb268e2990431ae0de83124
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-12-18 13:07:06 -0500

    init

--- pyteam/__init__.py
+++ pyteam/__init__.py
@@ -1,7 +1,11 @@
+from pyteam.agent_tooling import SpecialistBackedAgent
 from pyteam.fleet_retrievers import MultiVectorFleetRetriever
+from pyteam.fleet_specialists import FleetBackedSpecialist
 
 __version__ = "0.0.1"
 
 __all__ = [
+    "FleetBackedSpecialist",
     "MultiVectorFleetRetriever",
+    "SpecialistBackedAgent",
 ]
